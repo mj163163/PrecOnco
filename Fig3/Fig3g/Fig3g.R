d9de003bd1f88baa154778f3740d28c0e89bb4f6
Smriti Chawla <chawlasmriti_1990@yahoo.co.in>
2021-11-15 20:37:01 +0530

    Update Fig3g.R

--- Fig3/Fig3g/Fig3g.R
+++ Fig3/Fig3g/Fig3g.R
@@ -1,5 +1,4 @@
 ##loading libraries
-library(ggridges)
 library(ggplot2)
 
 ##loading predictions
@@ -16,14 +15,9 @@ colnames(metadata)[1] = "variable"
 
 ##Preparing data
 df = merge(pred,metadata,by="variable")
-
-
-
 df$Samples <- factor(df$Samples, levels = c("PRE-CX", "POST-CX","CRPC","ENZS" ,"ENZR"))
 
-
-
-library(ggplot2)
+##plotting
 ggplot(df, aes(x=Samples, y=value,col=Samples)) + 
   geom_boxplot(outlier.shape = NA)+scale_color_manual(values=c("PRE-CX"="#984EA3","POST-CX"="#FF7F00",CRPC="#E41A1C",ENZS="#4DAF4A",ENZR="#377EB8"))+theme_classic(base_size = 20)+theme(axis.text.x = element_text(angle = 45, hjust=1))+ylab("Predicted Z-score (IC50)")
 
